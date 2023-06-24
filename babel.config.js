f1017bcb3d5af0206fa41575e5ef1814112d1cf8
Filipe Silva <filipe.silva@nearsofsolutions.com>
2023-06-24 21:28:09 +0100

    Added qr code packages and configuration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'react-native-reanimated/plugin', // https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/
+      {
+        globals: ['__scanCodes'], // https://github.com/rodgomesc/vision-camera-code-scanner
+      },
+    ],
+  ],
 };
