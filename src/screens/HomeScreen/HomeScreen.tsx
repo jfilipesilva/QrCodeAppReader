f0af825fed0a95bd537987f76eeb2b15d8f13c39
Filipe Silva <filipe.silva@nearsofsolutions.com>
2023-06-24 12:57:12 +0100

    Added camera in screen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Linking, Text, View} from 'react-native';
+import {
+  Camera,
+  CameraPermissionStatus,
+  useCameraDevices,
+} from 'react-native-vision-camera';
 
 const HomeScreen: React.FunctionComponent = () => {
+  const [cameraPermissionStatus, setCameraPermissionStatus] =
+    useState<CameraPermissionStatus>('not-determined');
+
+  console.log('Requesting camera permission...');
+
+  Camera.requestCameraPermission().then(async permission => {
+    console.log(`Camera permission status: ${permission}`);
+
+    if (permission === 'denied') {
+      await Linking.openSettings();
+    }
+    setCameraPermissionStatus(permission);
+  });
+
+  const devices = useCameraDevices();
+  const device = devices.back;
   return (
-    <View>
+    <View style={{flex: 1}}>
       <Text>HomeScreen</Text>
+
+      {device && <Camera style={{flex: 1}} isActive={true} device={device} />}
     </View>
   );
 };
