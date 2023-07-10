60bace03a0ef77fca04ef5ed22d295870102a53b
Filipe Silva <filipe.silva@nearsofsolutions.com>
2023-07-10 13:01:56 +0100

    Added comments for Redux

--- src/store/slices/qr-codes/types.ts
+++ src/store/slices/qr-codes/types.ts
@@ -1,8 +1,19 @@
+/**
+ * The `QrCodesState` type represents the state of a QR code list with an ID counter.
+ * @property {QrCode[]} qrCodeList - This property stores the list of QRcodes.
+ * @property {number} idCounter - Keeps track of the current value for generating unique IDs for QR codes.
+ */
 export type QrCodesState = {
   qrCodeList: QrCode[];
   idCounter: number;
 };
 
+/**
+ * Represents a QR code object with an id, qrCode string, and date string.
+ * @property {number} id - A unique identifier for the QR code.
+ * @property {string} qrCode - Represents the QR code value.
+ * @property {string} date - Represents the date when the QR code was generated
+ */
 export type QrCode = {
   id: number;
   qrCode: string;
