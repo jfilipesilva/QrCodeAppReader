668ddc61a06ddbe810db27362063a21505868ed3
Filipe Silva <filipe.silva@nearsofsolutions.com>
2023-07-10 12:44:41 +0100

    Added comments for HistoryScreen

--- src/screens/HistoryScreen/HistoryScreen.tsx
+++ src/screens/HistoryScreen/HistoryScreen.tsx
@@ -13,15 +13,21 @@ import {QrCode} from '../../store/slices/qr-codes/types';
 import styles from './HistoryScreen.style';
 
 const HistoryScreen: React.FunctionComponent = () => {
-  const dispatch = useAppDispatch();
+  // Get the current list of QR codes from the Redux state
   const {qrCodeList} = useAppSelector(state => state.qrCodes);
 
+  // States
   const [qrCodeSelected, setQrCodeSelected] = useState<QrCode | undefined>(
     undefined,
   );
 
+  // Uses `useAppDispatch` hook to get the dispatch function from the Redux store
+  const dispatch = useAppDispatch();
+
+  // This ref will be used to reference the `BottomSheetModal` component in the code
   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
 
+  // Dispatch an action to remove it from the list and dismiss the BottomSheetModal
   const onPressButton = () => {
     if (qrCodeSelected) {
       dispatch(qrCodesSlice.actions.removeQrCode(qrCodeSelected));
@@ -29,13 +35,16 @@ const HistoryScreen: React.FunctionComponent = () => {
     bottomSheetModalRef.current?.dismiss();
   };
 
+  // When a list item is pressed shows a modal with the QRCode details
   const onPressListItem = (item: QrCode) => {
     setQrCodeSelected(item);
     bottomSheetModalRef.current?.present();
   };
 
+  // Renders HistoryScreen layout
   return (
     <ScreenLayout title={'Scan History'}>
+      {/* Only renders the searchable list if exist at least one QRCode  */}
       {qrCodeList.length > 0 ? (
         <SearchableList data={qrCodeList} onPressListItem={onPressListItem} />
       ) : (
