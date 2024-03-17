ce02226bfbb7d6a51b30efc31fb4c2c5ddf693cb
Filipe Silva <filipe.silva@yacoobalabs.com>
2024-03-17 11:28:12 +0000

    chore: upgraded rn version to 0.73.5

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,9 @@
 module.exports = {
   presets: [
-    'module:metro-react-native-babel-preset',
-    ['@babel/preset-env', {targets: {node: 'current'}}],
+    'module:@react-native/babel-preset',
+    // ['@babel/preset-env', {targets: {node: 'current'}}],
   ],
   plugins: [
-    [
-      'react-native-reanimated/plugin', // https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/
-      {
-        globals: ['__scanCodes'], // https://github.com/rodgomesc/vision-camera-code-scanner
-      },
-    ],
+    'react-native-reanimated/plugin', // https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/
   ],
 };
